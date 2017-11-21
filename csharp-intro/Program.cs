e5c8076914e40171afff4ec5e13b6e0bcaa6831a
John Furtado <jf8846@gmail.com>
2017-11-21 18:56:48 -0500

    Type conversion

--- csharp-intro/Program.cs
+++ csharp-intro/Program.cs
@@ -10,6 +10,19 @@ namespace csharp_intro
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //var number = "1234";
+                //byte b = Convert.ToByte(number);
+                //Console.WriteLine(b);
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
         }
     }
 }
