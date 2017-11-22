d2775aab6e9f011e1fb2305d04a33f0d6ca90763
John Furtado <jf8846@gmail.com>
2017-11-21 19:13:44 -0500

    Operators

--- csharp-intro/Program.cs
+++ csharp-intro/Program.cs
@@ -10,6 +10,11 @@ namespace csharp_intro
     {
         static void Main(string[] args)
         {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            
+            Console.WriteLine(c > b || c == a);
         }
     }
 }
