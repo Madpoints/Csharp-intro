89f7f71e7f5320da3fcff02c038520438ff83131
John Furtado <jf8846@gmail.com>
2017-11-26 17:42:18 -0500

    Reference Types part 2

--- csharp-intro/Program.cs
+++ csharp-intro/Program.cs
@@ -7,25 +7,31 @@ using System.Threading.Tasks;
 
 namespace csharp_intro
 {
-    public enum ShippingMethod
+    public class APerson
     {
-        RegularAirMail = 1,
-        RegisteredAirMail = 2,
-        Express = 3
+        public int Age;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var a = 10;
-            var b = a;
-            b++;
-            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
 
-            var array1 = new int[3] { 1, 2, 3 };
-            var array2 = array1;
-            array2[0] = 0;
-            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+            var person = new APerson() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(APerson person)
+        {
+            person.Age += 10;
         }
     }
 }
