0e39e602caa315294df00552f1a2baa802b99958
John Furtado <jf8846@gmail.com>
2017-11-30 18:18:44 -0500

    Creating Classes and Methods

--- csharp-intro/Program.cs
+++ csharp-intro/Program.cs
@@ -1,9 +1,5 @@
-﻿using csharp_intro.Math;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace csharp_intro
 {
@@ -11,31 +7,9 @@ namespace csharp_intro
     {
         static void Main(string[] args)
         {
-            var fullName = "Geralt of Rivia ";
-            Console.WriteLine("Trim: '{0}'", fullName.Trim());
-            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
-
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            Console.WriteLine("FirstName: " + firstName);
-            Console.WriteLine("LastName: " + lastName);
-
-            var names = fullName.Split(' ');
-            Console.WriteLine("FirstName: " + names[0]);
-            Console.WriteLine("LastName: {0} {1}", names[1] ,names[2]);
-
-            Console.WriteLine(fullName.Replace("of Rivia", "the White Wolf"));
-
-            if (String.IsNullOrWhiteSpace(" "))
-                Console.WriteLine("Invalid");
-
-            var str = "25";
-            var age = Convert.ToByte(str);
-            Console.WriteLine(age);
-
-            float price = 29.95f;
-            Console.WriteLine(price.ToString("C0"));
+            var sentence = "This is going to be a really really really really really long text.";
+            var summary = StringUtility.SummerizeText(sentence, 25);
+            Console.WriteLine(summary);
         }
     }
 }
