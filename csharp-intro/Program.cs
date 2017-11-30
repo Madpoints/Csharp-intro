146b2d7dbf804cb7d8b5e51966f1f016e271ab5e
John Furtado <jf8846@gmail.com>
2017-11-30 18:40:00 -0500

    StringBuilder

--- csharp-intro/Program.cs
+++ csharp-intro/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text;
 
 namespace csharp_intro
 {
@@ -7,9 +7,20 @@ namespace csharp_intro
     {
         static void Main(string[] args)
         {
-            var sentence = "This is going to be a really really really really really long text.";
-            var summary = StringUtility.SummerizeText(sentence, 25);
-            Console.WriteLine(summary);
+            var builder = new StringBuilder("Hello World");
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("  Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First char: " + builder[0]);
         }
     }
 }
