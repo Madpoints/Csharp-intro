39ba241429c571b4169d62742925647ccc522981
John Furtado <jf8846@gmail.com>
2017-12-05 18:42:24 -0500

    Debugging in Visual Studio

--- csharp-intro/Program.cs
+++ csharp-intro/Program.cs
@@ -9,14 +9,38 @@ namespace csharp_intro
     {
         static void Main(string[] args)
         {
-            var path = @"C:\Projects\Csharp-Intro\HelloWorld\HelloWorld.sln";
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 3);
 
-            Console.WriteLine("Extension Path: " + Path.GetExtension(path));
-            Console.WriteLine("File Name: " + Path.GetFileName(path));
-            Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
-
+            foreach (var number in smallests)
+                Console.WriteLine(number);
         }
 
+        public static List<int> GetSmallests(List<int> list, int count) 
+        {
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallests(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallests(List<int> list)
+        {
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+
+            return min;
+        }
     }
 }
